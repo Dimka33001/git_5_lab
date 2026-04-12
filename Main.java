42c2a49c3ed4b36e60677802473a7b63e6ec09a9
Ильичев Дмитрий <ili4off06@gmail.com>
2026-04-12 21:07:58 +0300

    Второй доп. коммит в main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Первый доп. коммит в main");
+        System.out.println("Второй доп. коммит в main");
     }
 }
