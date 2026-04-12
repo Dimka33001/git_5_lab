efad013ea5291a1a7476b4709e5f916720b54d51
Ильичев Дмитрий <ili4off06@gmail.com>
2026-04-12 21:23:27 +0300

    Второй коммит в dev

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Первый коммит в dev");
+        System.out.println("Второй коммит в dev");
     }
 }
